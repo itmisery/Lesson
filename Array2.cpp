af70c8673b75f156a89c1c68ebb808a7e69a7601
itmisery <scottishmike1995@gmail.com>
2021-03-20 12:28:50 +0200

    Add Array2.cpp

---
+++ Array2.cpp
@@ -0,0 +1,33 @@
+﻿// Array2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int const ROWS = 7;
+    int const COLS = 9;
+
+    int arr[ROWS][COLS];
+
+    for (int i = 0; i < ROWS; i++)
+    {
+        for (int j = 0; j < COLS; j++)
+        {
+            arr[i][j] = rand() % 30;
+        }
+    }
+    for (int i = 0; i < ROWS; i++)
+    {
+        for (int j = 0; j < COLS; j++)
+        {
+            cout << arr[i][j] << "\t";
+        }
+        cout << endl;
+    }
+    cin.get();
+    return 0;
+}
+
